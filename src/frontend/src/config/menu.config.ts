2b7de00da49428e40629234ead54f26f62dc81a9
Flexberry-man <mail@flexberry.net>
2026-07-30 07:46:49 +0000

    Flexberry code generation

--- src/frontend/src/config/menu.config.ts
+++ src/frontend/src/config/menu.config.ts
@@ -13,4 +13,25 @@ export const MENU_CONFIG: IMenuItem[] = [
     path: ROUTES_CONFIG.MAIN,
     icon: 'icon-point',
   },
+  {
+    title: 'NewFolder1',
+    path: '',
+    children: [
+      {
+        title: 'ClassTestL',
+        path: ROUTES_CONFIG.CLASS_TEST_L,
+        icon: 'icon-point',
+      },
+      {
+        title: 'AggregationClassL',
+        path: ROUTES_CONFIG.AGGREGATION_CLASS_L,
+        icon: 'icon-point',
+      },
+      {
+        title: 'AssosiationClassL',
+        path: ROUTES_CONFIG.ASSOSIATION_CLASS_L,
+        icon: 'icon-point',
+      },
+    ],
+  },
 ];
